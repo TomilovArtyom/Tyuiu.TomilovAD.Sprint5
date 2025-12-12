0780ef2af6092d007b454888ed3e892a36b967f6
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-12-13 04:11:01 +0500

    Task2

---
+++ Tyuiu.TomilovAD.Sprint5.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.TomilovAD.Sprint5.Task2.V15.Lib;
+using System.IO;
+
+namespace Tyuiu.TomilovAD.Sprint5.Task2.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint5.Task2.V15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using Tyuiu.TomilovAD.Sprint5.Task2.V15.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint5.Task2.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[3, 3] { { 6, 1, 7 }, { 1, 8, 5 }, { 6, 6, 4 } };
+
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int colums = mtrx.Length / rows;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{ mtrx[i, j]} \t");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл " + res);
+            Console.WriteLine("Создан");
+
+            Console.ReadKey();
+
+        }
+    }
+}
