26491937382fe3ee3cb00da53d475309d0690559
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-12-13 04:40:52 +0500

    Task3

---
+++ Tyuiu.TomilovAD.Sprint5.Task3.V8.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.TomilovAD.Sprint5.Task3.V8.Lib
+{
+    public class DataService : ISprint5Task3V8
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+
+            double y = Math.Round(((Math.Pow(x,2) + 1) / (3 * x + 4)), 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), System.Text.Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            
+            return path;
+        }
+    }
+}
--- Tyuiu.TomilovAD.Sprint5.Task3.V8/Program.cs
+++ Tyuiu.TomilovAD.Sprint5.Task3.V8/Program.cs
@@ -1,43 +1,31 @@
 ﻿using System.IO;
-using Tyuiu.TomilovAD.Sprint5.Task2.V15.Lib;
+using Tyuiu.TomilovAD.Sprint5.Task3.V8.Lib;
 
-namespace Tyuiu.TomilovAD.Sprint5.Task2.V15
+namespace Tyuiu.TomilovAD.Sprint5.Task3.V8
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] mtrx = new int[3, 3] { { 6, 1, 7 }, { 1, 8, 5 }, { 6, 6, 4 } };
-
-            int rows = mtrx.GetUpperBound(0) + 1;
-            int colums = mtrx.Length / rows;
+            int x = 3;
 
             DataService ds = new DataService();
 
             Console.Title = "Спринт #5 | Выполнил Томилов А. Д. | ИСТНб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Задание #2                                                              *");
-            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
             Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("Массив");
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < colums; j++)
-                {
-                    Console.Write($"{ mtrx[i, j]} \t");
-                }
-
-                Console.WriteLine();
-            }
+            Console.WriteLine("x = " + x);
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            string res = ds.SaveToFileTextData(mtrx);
+            string res = ds.SaveToFileTextData(x);
 
             Console.WriteLine("Файл " + res);
             Console.WriteLine("Создан");
