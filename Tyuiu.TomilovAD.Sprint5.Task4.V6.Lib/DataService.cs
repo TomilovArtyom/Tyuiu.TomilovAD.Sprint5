aaeee11cf84cb0781258e1791e8f9c5125a5a754
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-12-13 05:20:29 +0500

    Task4

--- Tyuiu.TomilovAD.Sprint5.Task4.V6.Lib/DataService.cs
+++ Tyuiu.TomilovAD.Sprint5.Task4.V6.Lib/DataService.cs
@@ -8,6 +8,7 @@ namespace Tyuiu.TomilovAD.Sprint5.Task4.V6.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
+            strx = strx.Replace(".", ",");
             double x = Convert.ToDouble(strx);
             double y = Math.Round((1 / Math.Cos(x) + 2.2 * Math.Pow(x, 2)), 3);
             return y;
