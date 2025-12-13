cd5823b15787f4209001f2b8dbfb7b664a9b60b4
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-12-13 05:18:03 +0500

    Task4

---
+++ Tyuiu.TomilovAD.Sprint5.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.TomilovAD.Sprint5.Task4.V6.Lib
+{
+    public class DataService : ISprint5Task4V6
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strx = File.ReadAllText(path);
+            double x = Convert.ToDouble(strx);
+            double y = Math.Round((1 / Math.Cos(x) + 2.2 * Math.Pow(x, 2)), 3);
+            return y;
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint5.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TomilovAD.Sprint5.Task4.V6.Lib;
+using System.IO;
+
+namespace Tyuiu.TomilovAD.Sprint5.Task4.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
