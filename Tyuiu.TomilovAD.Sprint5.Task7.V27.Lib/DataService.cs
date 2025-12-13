1a78d4e02d14222c0144412ace75960653112be7
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-12-13 08:54:23 +0500

    Task6

---
+++ Tyuiu.TomilovAD.Sprint5.Task7.V27.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.TomilovAD.Sprint5.Task7.V27.Lib
+{
+    public class DataService : ISprint5Task7V27
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathsavefile = Path.Combine("C:", "DataSprint5", "InPutDataFileTask7V27.txt");
+            FileInfo fileinfo = new FileInfo(pathsavefile);
+            bool fileExists = fileinfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathsavefile);
+            }
+
+            string strLine = "";
+            using ()
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint5.Task7.V27/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.TomilovAD.Sprint5.Task7.V27.Lib;
+using System.IO;
+
+namespace Tyuiu.TomilovAD.Sprint5.Task7.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask6V10.txt");
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Данные находятся в файле " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadDataAndSave(path);
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
